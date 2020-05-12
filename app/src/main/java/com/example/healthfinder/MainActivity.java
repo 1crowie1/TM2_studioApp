dd4300ac1740e2d638f45681ed6ef1544059b262
CormacH4 <cormac.hegarty@student.uts.edu.au>
2020-05-12 11:57:35 +1000

    Added new details for database storage of consultations and prescriptions. Removed reset button for testing. Created framework for Online Chat

--- app/src/main/java/com/example/healthfinder/MainActivity.java
+++ app/src/main/java/com/example/healthfinder/MainActivity.java
@@ -1,27 +1,26 @@
 package com.example.healthfinder;
 
 import android.app.Activity;
-import android.content.Context;
+
 import android.content.Intent;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.healthfinder.R;
-import com.google.android.gms.auth.api.Auth;
+
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.GoogleApiClient;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
@@ -30,10 +29,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.List;
-import java.util.concurrent.Callable;
 
 public class MainActivity extends AppCompatActivity{
 
--- app/src/main/java/com/example/healthfinder/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/healthfinder/ui/dashboard/DashboardFragment.java
@@ -11,18 +11,47 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.healthfinder.R;
+import com.example.healthfinder.entities.Consultation;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class DashboardFragment extends Fragment {
 
+    private RecyclerView chatList;
+    private TextView activeText;
 
+    private List<Consultation> chats;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         LayoutInflater lf = getActivity().getLayoutInflater();
         View view =  lf.inflate(R.layout.fragment_dashboard, container, false);
 
+        chatList = (RecyclerView) view.findViewById(R.id.chatList);
+        activeText = (TextView) view.findViewById(R.id.activeText);
+        chats = new ArrayList<Consultation>();
         return view;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(chats.isEmpty()){
+            chatList.setVisibility(View.GONE);
+            activeText.setVisibility(View.VISIBLE);
+            activeText.setText("No Active Chats");
+        }
+        else{
+            chatList.setVisibility(View.VISIBLE);
+            activeText.setVisibility(View.GONE);
+        }
+    }
+
 }
